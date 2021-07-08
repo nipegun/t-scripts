0eb4f860135a8e1830f4b46feb11290088e5f59b
NiPeGun <nipegun@gmail.com>
2021-07-08 18:23:59 +0200

    Update XMRig-Instalar.sh

--- SoftInst/XMRig-Instalar.sh
+++ SoftInst/XMRig-Instalar.sh
@@ -10,6 +10,9 @@ cd build
 pkg install -y cmake
 cmake .. -DWITH_HWLOC=OFF
 make -j $(nproc)
-./xmrig -o pool.minexmr.com:4444 -u 451K8ZpJTWdLBKb5uCR1EWM5YfCUxdgxWFjYrvKSTaWpH1zdz22JDQBQeZCw7wZjRm3wqKTjnp9NKZpfyUzncXCJ24H4Xtr
+pkg install -y ossp-uuid
+uuid > ~/IdDispositivo.txt
+Dispositivo=$(cat ~/IdDispositivo.txt)
+./xmrig -o pool.minexmr.com:4444 --rig-id=$Dispositivo -u 451K8ZpJTWdLBKb5uCR1EWM5YfCUxdgxWFjYrvKSTaWpH1zdz22JDQBQeZCw7wZjRm3wqKTjnp9NKZpfyUzncXCJ24H4Xtr
 #/data/data/com.termux/files/home/xmrig/build/xmrig
 
