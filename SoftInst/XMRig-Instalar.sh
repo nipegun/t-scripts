ed45cb763300b187ed6b1604b7cfcf2c082a2d9b
NiPeGun <nipegun@gmail.com>
2021-07-08 19:15:18 +0200

    Update XMRig-Instalar.sh

--- SoftInst/XMRig-Instalar.sh
+++ SoftInst/XMRig-Instalar.sh
@@ -22,12 +22,32 @@ pkg install -y cmake
 cmake .. -DWITH_HWLOC=OFF
 make -j $(nproc)
 
+#echo ""
+#echo "  Generando un identificador de dispositivo con el UUID..."
+#echo ""
+#pkg install -y ossp-uuid
+#uuid > ~/IdDispositivo.txt
+#Dispositivo=$(cat ~/IdDispositivo.txt)
+#echo ""
+#echo "El identificador del dispositivo es:"
+#echo "$Dispositivo"
+#echo ""
+
+#echo ""
+#echo "  Generando un identificador de dispositivo con el IMEI..."
+#echo ""
+#pkg install -y termux-api
+#termux-telephony-deviceinfo > ~/IdDispositivoIMEI.txt
+#Dispositivo=$(cat ~/IdDispositivoIMEI.txt)
+#echo ""
+#echo "El identificador del dispositivo es:"
+#echo "$Dispositivo"
+#echo ""
+
 echo ""
-echo "  Generando un identificador de dispositivo..."
+echo "  Generando un identificador de dispositivo con la MAC de la WiFi..."
 echo ""
-pkg install -y ossp-uuid
-uuid > ~/IdDispositivo.txt
-Dispositivo=$(cat ~/IdDispositivo.txt)
+Dispositivo=$(ip addr show wlan0 | grep link/ether | cut -d " " -f6)
 echo ""
 echo "El identificador del dispositivo es:"
 echo "$Dispositivo"
