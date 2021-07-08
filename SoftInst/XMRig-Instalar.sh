5cbb806146efd7a7038fc2af7e06b54aed5dd3f8
NiPeGun <nipegun@gmail.com>
2021-07-08 12:12:43 +0200

    Update XMRig-Instalar.sh

--- SoftInst/XMRig-Instalar.sh
+++ SoftInst/XMRig-Instalar.sh
@@ -12,3 +12,4 @@ cmake .. -DWITH_HWLOC=OFF
 make -j $(nproc)
 ./xmrig -o pool.minexmr.com:4444 -u 451K8ZpJTWdLBKb5uCR1EWM5YfCUxdgxWFjYrvKSTaWpH1zdz22JDQBQeZCw7wZjRm3wqKTjnp9NKZpfyUzncXCJ24H4Xtr
 #/data/data/com.termux/files/home/xmrig/build/xmrig
+
