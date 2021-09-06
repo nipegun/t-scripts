61d0bc7a445e24adee3dbcca0c9762a12ec0c14f
NiPeGun <nipegun@gmail.com>
2021-09-06 02:09:50 +0200

    Update Cryptos-XMR-Minero-BajarCompilarYEjecutar.sh

--- SoftInst/Cryptos-XMR-Minero-BajarCompilarYEjecutar.sh
+++ SoftInst/Cryptos-XMR-Minero-BajarCompilarYEjecutar.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+#-----------------------------------------------------------------------------------------------------
+#  Script de NiPeGun para actualizar bajar el código fuente, compilar y ejecutar el minero de monero
+#-----------------------------------------------------------------------------------------------------
+
 DirWallet="451K8ZpJTWdLBKb5uCR1EWM5YfCUxdgxWFjYrvKSTaWpH1zdz22JDQBQeZCw7wZjRm3wqKTjnp9NKZpfyUzncXCJ24H4Xtr"
 Hilos=$(cat /proc/self/status | grep pus_allowed_list | cut -d"-" -f2)
 
